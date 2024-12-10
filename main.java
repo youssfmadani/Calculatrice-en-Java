b648c25d3582fe6fbe566c79a5ed80d2f0ad904b
youssfmadani <youssfelmadani@gmail.com>
2024-12-10 11:03:36 +0100

    Créer un menu pour présenter les options disponibles.

--- main.java
+++ main.java
@@ -2,7 +2,25 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean continuer = true;
+
+        while (continuer) {
+            // Menu principal
+            System.out.println("\n--- Menu Principal ---");
+            System.out.println("1. Addition (+)");
+            System.out.println("2. Soustraction (-)");
+            System.out.println("3. Multiplication (*)");
+            System.out.println("4. Division (/)");
+            System.out.println("5. Puissance (^)");
+            System.out.println("6. Racine carrée (√)");
+            System.out.println("7. Factorielle (!)");
+            System.out.println("8. Quitter");
+            System.out.print("Choisissez une opération : ");
+            int choix = scanner.nextInt();
 
 
-    }
+
+
+        }
 }
